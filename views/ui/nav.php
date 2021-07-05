978e8445154a6f3f217dd7163886c462997f9ad0
sandakelum-dev <sandakelum.dev@gmail.com>
2021-07-05 09:28:55 +0530

    simple route

---
+++ views/ui/nav.php
@@ -0,0 +1,11 @@
+<div class="nav-bar">
+    <div class="left-nav-items">
+        <ul>
+            <li>LOGO</li>
+            <li><a href="/?page=home">Home</a> </li>
+            <li><a href="/?page=top10"> Top 10</a></li>
+        </ul>
+    </div>
+    <div class="right-nav-items">
+    </div>
+</div>
