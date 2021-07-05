422b4919d878893bf8b43e878fbe79e2ba5ff6e3
sandakelum-dev <sandakelum.dev@gmail.com>
2021-07-05 20:14:29 +0530

    some styles

---
+++ views/pages/register.php
@@ -0,0 +1,42 @@
+<div class="container ptb-80">
+    <div class="row ">
+        <div class="col-12">
+            <h1 class="title">Register</h1>
+        </div>
+    </div>
+    <form action="" method="post">
+        <div class="row">
+            <div class="col-3">
+                <div class="row">
+                    <div>
+                        <h1>image</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <br>
+                    <input id="name" name="name" placeholder="john doe" type="text">
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <br>
+                            <input id="name" name="name" placeholder="john doe" type="text">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <br>
+                            <input id="name" name="name" placeholder="john doe" type="text">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </form>
+</div>
--- views/ui/nav.php
+++ views/ui/nav.php
@@ -1,11 +1,17 @@
 <div class="nav-bar">
     <div class="left-nav-items">
-        <ul>
-            <li>LOGO</li>
-            <li><a href="/?page=home">Home</a> </li>
-            <li><a href="/?page=top10"> Top 10</a></li>
+        <ul class="nav-item-group">
+            <li>Any<i class="fa fa-pencil" aria-hidden="true"></i>Write</li>
+            <li><a href="/?page=home"><i class="fa fa-home" aria-hidden="true"></i> Home</a> </li>
+            <li><a href="/?page=top10"><i class="fa fa-star" aria-hidden="true"></i> Top 10</a></li>
         </ul>
     </div>
     <div class="right-nav-items">
+    <ul class="nav-item-group">
+            <!-- <li>Any<i class="fa fa-pencil" aria-hidden="true"></i>Write</li> -->
+            <li><a href="/?page=login">Loing</a> </li>
+            <li><a href="/?page=register"> Reister</a></li>
+        </ul>
     </div>
 </div>
+<br>
