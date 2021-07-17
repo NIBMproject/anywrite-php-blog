1f62e2bbe9280f242142cf46c58b74c4fbdd4fc4
sandakelum-dev <sandakelum.dev@gmail.com>
2021-07-17 14:46:03 +0530

    add nav

--- views/ui/nav.php
+++ views/ui/nav.php
@@ -1,21 +1,42 @@
-<div class="nav-bar">
-    <nav>
-        <p>AnyWrite</p>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Top 10</a></li>
+<div class="topnav" id="myTopnav">
+      <div class="logo">
+        <h1>AnyWrite</h1>
+      </div>
 
-            <li><a href="">username</a>
-                <ul>
-                    <li><a href="">New Article</a></li>
-                    <li><a href="">Profile</a></li>
-                    <hr>
-                    <li><a href="">Logout</a></li>
-                </ul>
-            </li>
-            <li><a href="">Log in</a></li>
-            <li><a href="">Register</a></li>
-        </ul>
-    </nav>
-</div>
-<br>
+      <div class="nav">
+        <a id="logo-link" href="#">AnyWrite</a>
+        <a href="#home">Home</a>
+        <a href="#news">Top 10</a>
+        <a href="#contact">Login</a>
+        <div class="dropdown">
+          <button class="dropbtn">
+            Username
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">Profile</a>
+            <a href="#">My article</a>
+            <a href="#">Logout</a>
+          </div>
+        </div>
+        <a
+          href="javascript:void(0);"
+          style="font-size: 15px"
+          class="icon"
+          onclick="myFunction()"
+          >&#9776;</a
+        >
+      </div>
+    </div>
+
+    <script>
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+    </script>
