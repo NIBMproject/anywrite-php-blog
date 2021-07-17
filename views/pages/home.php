9a7b4d5d6e555655e484892ecde089c023865fbf
Sameera Madushan <sameera.xyz.me@gmail.com>
2021-07-17 13:27:01 +0530

    Added article mockups

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,15 +1,23 @@
 <div class="articles">
-    <div>
-        <div>
-            <h1>
-                post1
-            </h1>
+    <div class="container">
+        <div class="box-1">
+            <h2>TITLE HEADING</h2>
+            <div class="fakeimg" style="height:200px;">Image 1</div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem unde inventore debitis quisquam maiores reiciendis vel magnam at pariatur error?Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor facilis labore quod fuga molestiae voluptatibus illo consectetur optio suscipit.</p>
+            <a href="#" class=btn>Read More</a>
         </div>
-        <div>
-            <p>fdfds fdgfdgfdg</p>
+        <div class="box-2">
+            <h2>TITLE HEADING</h2>
+            <div class="fakeimg" style="height:200px;">Image 2</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur laudantium quae vero cupiditate id perspiciatis corporis iure incidunt unde tempora. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero at iusto quasi possimus, non est doloremque illo inventore nam distinctio.</p>
+            <a href="#" class=btn>Read More</a>
         </div>
-        <div>
-
+        <div class="box-3">
+            <h2>TITLE HEADING</h2>
+            <div class="fakeimg" style="height:200px;">Image 3</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias alias distinctio error ipsam architecto cumque eos perspiciatis cupiditate, illo debitis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae voluptates molestiae officia esse aut, dolores possimus nulla nihil soluta?</p>
+            <a href="#" class=btn>Read More</a>
         </div>
     </div>
+
 </div>
