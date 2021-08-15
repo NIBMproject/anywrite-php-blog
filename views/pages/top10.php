cb316d6b26d6533ace24c6df54a5c788cc1eebfc
sandakelum-dev <sandakelum.dev@gmail.com>
2021-08-15 16:47:16 +0530

    fix

--- views/pages/top10.php
+++ views/pages/top10.php
@@ -1,9 +1,7 @@
 
 <div class="container ptb-80">
    <div class="row">
-       <div class="col-12">
-<!--          <h1>TOP 10</h1>
-         <br><br> -->
+       <div class="col-8">
          <?php
           $db = new Db();
           $util = new Util();
@@ -13,10 +11,10 @@
                 while ($row = $top->fetch_assoc()) { ?>
                     <div class="row article-card ">
                         <div class="row">
-                            <div class="col-1">
+                            <div class="col-2">
                                 <img class="article-user-image" src="<?php echo  $db->findDataById("user",$row['user_id'],"img_path");?>">
                             </div>
-                            <div class="col-11 article-user-data">
+                            <div class="col-10 article-user-data">
                                 <div class="row article-u-name"><?php echo $db->findDataById("user",$row['user_id'],"name"); ?></div>
                                 <div class="row article-p-time"><?php echo $util->getTimeDiff($row['createAt']);?></div>
                             </div>
@@ -32,6 +30,9 @@
                     <hr>
             <?php }
             }; ?>
+       </div>
+       <div class="col-4">
+           ads
        </div>
    </div>
 </div>
